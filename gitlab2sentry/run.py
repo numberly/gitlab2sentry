9b4e03d6bf8896733ae92ef252921537ff859b6b
Theofanis Petkos <theofanis.petkos@numberly.com>
2022-05-18 15:31:55 +0200

    Add run py as entrypoint

---
+++ gitlab2sentry/run.py
@@ -0,0 +1,13 @@
+import sentry_sdk
+from gitlab2sentry import Gitlab2Sentry
+from gitlab2sentry.resources import SENTRY_DSN, SENTRY_ENV
+
+
+if __name__ == "__main__":
+    sentry_sdk.init(
+        debug=False,
+        dsn=SENTRY_DSN,
+        environment=SENTRY_ENV,
+    )
+    runner = Gitlab2Sentry()
+    runner.update()
