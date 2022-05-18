33be603b6bce13aecd790135627ab0140369dce0
Theofanis Petkos <theofanis.petkos@numberly.com>
2022-05-18 18:39:52 +0200

    Implement SentryAPIClient

--- gitlab2sentry/utils/sentry_provider.py
+++ gitlab2sentry/utils/sentry_provider.py
@@ -1,112 +1,214 @@
 import logging
-from typing import Tuple
+import json
+from typing import Any, Dict, Optional, Tuple
 
 import requests
+from requests import Response
 from slugify import slugify
-from gitlab.v4.objects import Project
+from gitlab2sentry.exceptions import SentryProjectCreationFailed, SentryProjectKeyIDNotFound
+from gitlab2sentry.resources import SENTRY_URL
 
-class SentryProvider:
-    def __init__(self, url, *args, **kwargs):
-        self.url = url
-        self.token = kwargs["auth_token"]
-        self.org_slug = kwargs["org_slug"]
-        self.headers = {"Authorization": f"Bearer {self.token}"}
 
-    def __str__(self):
-        return "<SentryProvider>"
+class SentryAPIClient:
+    def __init__(
+        self,
+        token: str,
+        base_url: Optional[str]=SENTRY_URL,
+    ):
+        self.base_url = base_url
+        self.url = "{}/api/0/{}"
+        self.headers = {
+            "Authorization": f"Bearer {token}"
+        }
 
-    def get_projects(self):
-        url = f"{self.url}/api/0/projects/"
+    def __str__():
+        return "<SentryAPIClient>"
+
+    def _get_json(self, response: Response) -> Optional[Dict[str, Any]]:
+        try:
+            return response.status_code, response.json()
+        except json.JSONDecodeError as json_error:
+            logging.warning(
+                "{}: Error on request suffix: {}".format(
+                    self.__str__(), str(json_error)
+                )
+            )
+            return 400, None
+
+    def simple_request(
+        self,
+        method: str,
+        suffix: Optional[str]=None,
+        data: Optional[Dict[str, Any]]=None,
+    ) -> Dict[str, Any]:
+        url = self.url.format(
+            self.base_url, suffix
+        )
+        logging.debug(
+            "{} simple {} request to {}".format(
+                self.__str__(), method, url
+            )
+        )
+        if method == "post":
+            return self._get_json(
+                requests.post(
+                    url,
+                    data=data,
+                    headers=self.headers
+                )
+            )
+        elif method == "put":
+            return self._get_json(
+                requests.put(
+                    url,
+                    data=data,
+                    headers=self.headers
+                )
+            )
+        else:
+            return self._get_json(
+                requests.get(
+                    url,
+                    headers=self.headers
+                )
+            )
+
+    def page_request(self, suffix: str) -> Optional[Dict[str, Any]]:
+        url = self.url.format(self.base_url, suffix)
+        logging.debug(
+            "{} paginated get request to {}".format(
+                self.__str__(), url
+            )
+        )
         while True:
             r = requests.get(
                 url,
                 headers=self.headers,
             )
-            yield r.json()
-            if r.links["next"]["results"] == "true":
+            yield self._get_json(r)
+            if (hasattr(r, "links") and r.links
+                and r.links.get("next", None)
+                and r.links["next"].get("results", None)
+                and r.links["nex"]["results"] == "true"
+            ):
                 url = r.links["next"]["url"]
             else:
                 break
 
-    def _create_or_get_team(self, team):
-        team_slug = slugify(team)
-        r = requests.post(
-            f"{self.url}/api/0/organizations/{self.org_slug}/teams/",
-            headers=self.headers,
-            data={
-                "name": team,
+
+class SentryProvider:
+    def __init__(self, url: str, token: str, org_slug: str):
+        self.url = url
+        self.org_slug = org_slug
+        self._client = SentryAPIClient(token, SENTRY_URL)
+
+    def __str__(self):
+        return "<SentryProvider>"
+
+    def _create_or_get_team(self, team_name: str) -> Optional[Dict[str, Any]]:
+        team_slug = slugify(team_name)
+        status_code, result = self._client.simple_request(
+            "post",
+            "/organizations/{}/teams/".format(self.org_slug),
+            {
+                "name": team_name,
                 "slug": team_slug,
             },
         )
-        result = r.json()
-        if r.status_code != 201:
-            if r.status_code == 409:
-                r = requests.get(
-                    f"{self.url}/api/0/teams/{self.org_slug}/{team_slug}/",
-                    headers=self.headers,
+
+        if status_code == 409:
+            return self._client.simple_request(
+                "get",
+                "teams/{}/{}/".format(
+                    self.org_slug, team_slug
                 )
-                return r.json()
+            )[1]
+
+        if status_code != 201:
             return None
 
-        logging.info(f"{self.__str__()}team {team} created!")
+        logging.info(
+            "{}: Team {} created!".format(
+                self.__str__(), team_name
+            )
+        )
         return result
 
-    def get_project(self, team, project_slug):
-        r = requests.get(
-            f"{self.url}/api/0/projects/{self.org_slug}/{project_slug}/",
-            headers=self.headers,
+    def get_project(self, project_slug: str) -> Optional[Dict[str, Any]]:
+        status_code, result = self._client.simple_request(
+            "get",
+            "projects/{}/{}/".format(
+                self.org_slug, project_slug
+            )
         )
-        if r.status_code != 200:
+        if status_code != 200:
             return None
-        return r.json()
+        return result
 
-    def create_or_get_project(self, team, project):
+    def create_or_get_project(self, team: str, project: str) -> Dict[str, Any]:
         project_slug = slugify(project).lower()
-        data = {
-            "name": project,
-            "slug": project_slug,
-        }
-        r = requests.post(
-            f"{self.url}/api/0/teams/{self.org_slug}/{team}/projects/",
-            headers=self.headers,
-            data=data,
+        status_code, result = self._client.simple_request(
+            "post",
+            "teams/{}/{}/projects/".format(
+                self.org_slug, team
+            ),
+            {
+                "name": project,
+                "slug": project_slug,
+            }
         )
-        result = r.json()
-        if r.status_code != 201:
-            if r.status_code == 409:
-                return self.get_project(team, project_slug)
-            raise Exception(result)
+        if status_code != 201:
+            if status_code == 409:
+                return self.get_project(project_slug)
+            raise SentryProjectCreationFailed(result)
 
         return result
 
-    def get_dsn_and_key_id(self, project_slug: str) -> Tuple[str, int]:
-        r = requests.get(
-            f"{self.url}/api/0/projects/{self.org_slug}/{project_slug}/keys/",
-            headers=self.headers,
+    def _get_dsn_and_key_id(self, project_slug: str) -> Tuple[str, int]:
+        status_code, result = self._client.simple_request(
+            "get",
+            "projects/{}/{}/keys/".format(
+                self.org_slug, project_slug
+            ),
         )
-        if r.status_code != 200:
+
+        if status_code != 200:
             return None
-        
-        response = r.json()
-        
-        return response[0]["dsn"]["public"], response[0]["id"]
+        if (
+            result
+            and len(result) > 0
+            and result[0].get("dsn", None)
+            and result[0]["dsn"].get("public", None)
+            and result[0].get("id", None)
+        ):
+            return result[0]["dsn"]["public"], result[0]["id"]
+        else:
+            raise SentryProjectKeyIDNotFound(result)
 
 
-    def set_rate_limit_for_key(self, project: str) -> str:
+    def set_rate_limit_for_key(self, project: str) -> Optional[str]:
         dsn, key = self._get_dsn_and_key_id(project)
-        data = {"rateLimit": {"window": 60, "count": 300}}
-        url = f"{self.url}/api/0/projects/{self.org_slug}/{project}/keys/{key}/"
-        r = requests.put(
-            f"{self.url}/api/0/projects/{self.org_slug}/{project}/keys/{key}/",
-            headers=self.headers,
-            json=data,
+        status_code, result = self._client.simple_request(
+            "put",
+            "projects/{}/{}/keys/{}/".format(
+                self.org_slug, project, key
+            ),
+            {
+                "rateLimit": {
+                    "window": 60,
+                    "count": 300
+                }
+            }
         )
-        if r.status_code != 200:
+
+        if status_code != 200:
             return None
         return dsn
 
-    def ensure_sentry_team(self, team_name: str):
+    def ensure_sentry_team(self, team_name: str) -> None:
         logging.info(
-            f"{self.__str__()}: Ensuring team {team_name} exists on sentry"
+            "{}: Ensuring team {} exists on sentry".format(
+                self.__str__(), team_name
+            )
         )
         self._create_or_get_team(team_name)
