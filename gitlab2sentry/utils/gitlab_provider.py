numberly/gitlab2sentry

commit 41342bc9267ed6576d27f245bd5e631e80ca3db9

fix(gitlab): ProjectMemberManager has no attribute "all"

gitlab2sentry/utils/gitlab_provider.py
@@ -221,7 +221,7 @@ class GitlabProvider:
         return ", ".join(
             [
                 f"@{member.username}"
-                for member in project.members.all()
+                for member in project.members.list()
                 if (
                     member.access_level >= GITLAB_MENTIONS_ACCESS_LEVEL
                     and member.state != "blocked"

tests/conftest.py
@@ -67,7 +67,7 @@ class TestGitlabProject:
 
 
 class TestGitlabMemberManager:
-    def all(self):
+    def list(self):
         return TEST_GITLAB_PROJECT_MEMBERS
 
 

tests/test_gitlab_provider.py
@@ -92,7 +92,7 @@ def test_get_default_mentions(gitlab_provider_fixture, gitlab_project_fixture):
     )
     _project_non_blocked_members = [
         member
-        for member in gitlab_project_fixture.members.all()
+        for member in gitlab_project_fixture.members.list()
         if member.state != "blocked"
     ]
     assert len(_mentioned_members) == len(_project_non_blocked_members)

commit 5a02134c32f7eb3d836e203cf4487a3dab480094

fix(gitlab_provider): update member retrieval method to include all members
regression from 41342bc9267ed6576d27f245bd5e631e80ca3db9

gitlab2sentry/utils/gitlab_provider.py
@@ -221,7 +221,7 @@ class GitlabProvider:
         return ", ".join(
             [
                 f"@{member.username}"
-                for member in project.members.list()
+                for member in project.members_all.list(get_all=True)
                 if (
                     member.access_level >= GITLAB_MENTIONS_ACCESS_LEVEL
                     and member.state != "blocked"

tests/conftest.py
@@ -63,11 +63,11 @@ TEST_GITLAB_PROJECT_MEMBERS = [
 
 class TestGitlabProject:
     def __init__(self):
-        self.members = TestGitlabMemberManager()
+        self.members_all = TestGitlabMemberManager()
 
 
 class TestGitlabMemberManager:
-    def list(self):
+    def list(self, *args, **kwargs):
         return TEST_GITLAB_PROJECT_MEMBERS
 
 

tests/test_gitlab_provider.py
@@ -92,7 +92,7 @@ def test_get_default_mentions(gitlab_provider_fixture, gitlab_project_fixture):
     )
     _project_non_blocked_members = [
         member
-        for member in gitlab_project_fixture.members.list()
+        for member in gitlab_project_fixture.members_all.list()
         if member.state != "blocked"
     ]
     assert len(_mentioned_members) == len(_project_non_blocked_members)